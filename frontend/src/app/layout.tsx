e64543cb932b31b6da00bcda7155ef452fc18b03
PedroHaggeBatista <pedro.baptista@sou.inteli.edu.br>
2025-02-02 01:45:16 -0300

    feat: maintence page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,37 +1,45 @@
+import type React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SolanaWalletProvider from "@/providers/SolanaWalletProvider";
 import { Toaster } from "react-hot-toast";
+import Maintenance from "@/components/Organisms/Maintence";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "SWQuery",
-	description: "SWQuery is a Solana-based data query system.",
+  title: "SWQuery",
+  description: "SWQuery is a Solana-based data query system.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				<SolanaWalletProvider>{children}</SolanaWalletProvider>
-				<Toaster position="top-center" reverseOrder={false} />
-			</body>
-		</html>
-	);
+  const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE === "true"
+
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {isMaintenanceMode ? (
+          <Maintenance />
+        ) : (
+          <>
+            <SolanaWalletProvider>{children}</SolanaWalletProvider>
+            <Toaster position="top-center" reverseOrder={false} />
+          </>
+        )}
+      </body>
+    </html>
+  )
 }
